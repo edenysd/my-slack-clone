7873f67566a5aa4e9886e192d1900226d98fd8a0
Edenys Deniz González <holofist1@gmail.com>
2023-08-22 14:59:28 -0400

    Create a layout for chat page

---
+++ src/layouts/ChatLayout.jsx
@@ -0,0 +1,136 @@
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import MailIcon from "@mui/icons-material/Mail";
+import InboxIcon from "@mui/icons-material/Inbox";
+import MenuIcon from "@mui/icons-material/Menu";
+import PropTypes from "prop-types";
+
+const drawerWidth = 240;
+
+const DrawerContent = () => (
+  <div>
+    <Toolbar />
+    <Divider />
+    <List>
+      {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+        <ListItem key={text} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+    <Divider />
+    <List>
+      {["All mail", "Trash", "Spam"].map((text, index) => (
+        <ListItem key={text} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  </div>
+);
+
+const ChatLayout = ({ children }) => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <AppBar
+          position="fixed"
+          sx={{
+            display: { xs: "block", sm: "none" },
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+          }}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Slack Clone
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          aria-label="mailbox folders"
+        >
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                backgroundColor: "#541554",
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+          >
+            <DrawerContent />
+          </Drawer>
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: "none", sm: "block" },
+              "& .MuiDrawer-paper": {
+                backgroundColor: "#541554",
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+            open
+          >
+            <DrawerContent />
+          </Drawer>
+        </Box>
+        {children}
+      </Box>
+    </>
+  );
+};
+
+ChatLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default ChatLayout;
--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,7 +1,12 @@
 import { Outlet } from "react-router";
+import ChatLayout from "../layouts/ChatLayout";
 
 const ChatPage = () => {
-  return <Outlet />;
+  return (
+    <ChatLayout>
+      <Outlet />
+    </ChatLayout>
+  );
 };
 
 export default ChatPage;
