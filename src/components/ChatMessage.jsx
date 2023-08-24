e35fe961cbea73575b061165bc4d042c349a24e3
Edenys Deniz González <holofist1@gmail.com>
2023-08-24 18:47:32 -0400

    Add ChatMessage component
    With view/edition/delete capabilities

---
+++ src/components/ChatMessage.jsx
@@ -0,0 +1,82 @@
+import PropTypes from "prop-types";
+import { Avatar, Box, IconButton } from "@mui/material";
+import { getFullName } from "../utils/userUtils";
+import { useState } from "react";
+import { Delete, Edit } from "@mui/icons-material";
+import ChatEditMessage from "./ChatEditMessage";
+
+const ChatMessage = ({
+  user,
+  timestamp,
+  text,
+  removable,
+  editable,
+  onEdit,
+  onRemove,
+}) => {
+  const [isEditMode, setEditMode] = useState(false);
+  return !isEditMode ? (
+    <Box className="flex group items-start w-full" sx={{ minHeight: "90px" }}>
+      <Avatar
+        className="mt-2"
+        variant="rounded"
+        src={user.avatar}
+        sx={{ width: "54px", height: "54px" }}
+      />
+      <div className="flex flex-col ml-3 w-full">
+        <div className="flex items-center justify-between">
+          <span>
+            <span className="font-bold text-base mr-2">
+              {getFullName(user)}
+            </span>
+            <span className="text-gray-500 text-sm">
+              {timestamp.calendar()}
+            </span>
+          </span>
+          <span className="group-hover:opacity-100 opacity-0 transition-all">
+            {editable ? (
+              <IconButton
+                sx={{ width: "32px", height: "32px" }}
+                onClick={() => setEditMode(true)}
+              >
+                <Edit color="primary" sx={{ width: "24px", height: "24px" }} />
+              </IconButton>
+            ) : null}
+            {removable ? (
+              <IconButton
+                sx={{ width: "32px", height: "32px" }}
+                onClick={onRemove}
+              >
+                <Delete color="error" sx={{ width: "24px", height: "24px" }} />
+              </IconButton>
+            ) : null}
+          </span>
+        </div>
+        <p
+          className="text-gray-700 whitespace-break-spaces"
+          style={{ overflowWrap: "break-word" }}
+        >
+          {text}
+        </p>
+      </div>
+    </Box>
+  ) : (
+    <ChatEditMessage
+      messageToEdit={text}
+      onCancelEdition={() => setEditMode(false)}
+      onEditMessage={onEdit}
+    />
+  );
+};
+
+ChatMessage.propTypes = {
+  user: PropTypes.object,
+  timestamp: PropTypes.object,
+  text: PropTypes.string,
+  onEdit: PropTypes.func,
+  onRemove: PropTypes.func,
+  removable: PropTypes.bool,
+  editable: PropTypes.bool,
+};
+
+export default ChatMessage;
