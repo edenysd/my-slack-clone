427061c24f9fd37f615ad0c771ba49b40d01b30d
Edenys Deniz González <holofist1@gmail.com>
2023-08-23 20:04:54 -0400

    Redirect to register page when user data not found

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router";
+import { useNavigate } from "react-router-dom";
 import ChatLayout from "../layouts/ChatLayout";
+import useUserStore from "../store/userStore";
 
 const ChatPage = () => {
+  const userStore = useUserStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userStore.currentUser.firstName) {
+      navigate("/register");
+    }
+  });
+
   return (
     <ChatLayout>
       <Outlet />
