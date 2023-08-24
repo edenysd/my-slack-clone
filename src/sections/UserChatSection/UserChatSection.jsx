d18dc4ad9ed1b2c255dfbc1a2058de23eb72fdd6
Edenys Deniz González <holofist1@gmail.com>
2023-08-24 17:20:16 -0400

    Fix Scroll behavior

--- src/sections/UserChatSection/UserChatSection.jsx
+++ src/sections/UserChatSection/UserChatSection.jsx
@@ -2,15 +2,17 @@ import useUserStore from "../../store/userStore";
 import ChatTextField from "../../components/ChatTextField";
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import useMessageStore from "../../store/messageStore";
+import usePrivateMessageStore from "../../store/privateMessageStore";
 import { Avatar, IconButton } from "@mui/material";
 import { WavingHand } from "@mui/icons-material";
 import ChatMessage from "../../components/ChatMessage";
 
 const UserChatSection = () => {
   const getUserById = useUserStore((state) => state.getUserById);
-  const messageStore = useMessageStore();
-  const privateMessages = useMessageStore((state) => state.privateMessages);
+  const privateMessageStore = usePrivateMessageStore();
+  const privateMessages = usePrivateMessageStore(
+    (state) => state.privateMessages
+  );
 
   const [targetUser, setTargetUser] = useState({});
   const currentUser = useUserStore((state) => state.currentUser);
@@ -20,13 +22,13 @@ const UserChatSection = () => {
 
   const sendMessage = useCallback(
     (text) => {
-      messageStore.sendNewPrivateMessage({
+      privateMessageStore.sendNewPrivateMessage({
         from: currentUser.id,
         to: targetUser.id,
         text,
       });
     },
-    [currentUser.id, messageStore, targetUser.id]
+    [currentUser.id, privateMessageStore, targetUser.id]
   );
 
   const sendHiMessage = useCallback(() => {
@@ -39,7 +41,7 @@ const UserChatSection = () => {
 
   useEffect(() => {
     const getData = async () => {
-      await messageStore.fetchAllPrivateMessages();
+      await privateMessageStore.fetchAllPrivateMessages();
     };
     getData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -47,7 +49,7 @@ const UserChatSection = () => {
 
   useEffect(() => {
     setCurrentPrivateMessages(
-      messageStore.filterAllMessagesFromPrivateConversation({
+      privateMessageStore.filterAllMessagesFromPrivateConversation({
         from: currentUser.id,
         to: targetUser.id,
       })
@@ -56,46 +58,59 @@ const UserChatSection = () => {
   }, [privateMessages, currentUser.id, targetUser.id]);
 
   return (
-    <main className="flex flex-col-reverse text-black w-full min-h-full bg-white px-6 py-16 sm:py-16 lg:px-8">
-      <ChatTextField
-        placeholder={`Send a message to ${targetUser?.username}`}
-        submitMessage={sendMessage}
-      />
-      {currentPrivateMessages.length ? (
-        currentPrivateMessages.map((message) => (
-          <ChatMessage
-            key={message.id}
-            user={getUserById(message.from)}
-            timestamp={message.timestamp}
-            text={message.text}
-          />
-        ))
-      ) : (
-        <div className="h-full w-full justify-center flex flex-col items-center">
-          <div className="font-semibold text-3xl flex flex-col text-black items-center whitespace-normal">
-            <span className="mb-4">Say Hi to </span>
-            <div className="flex flex-col items-center mb-4">
-              <Avatar
-                variant="rounded"
-                src={targetUser.avatar}
-                sx={{ width: "64px", height: "64px" }}
-              />
-              <span className="text-fuchsia-800 text-2xl">
-                {targetUser.username}
+    <main className="flex flex-col-reverse text-black w-full min-h-full bg-white py-16 sm:py-16">
+      <div className=" px-6 lg:px-8 ">
+        <ChatTextField
+          className="px-6 lg:px-8"
+          placeholder={`Send a message to ${targetUser?.username}`}
+          submitMessage={sendMessage}
+        />
+      </div>
+      <div className="overflow-auto h-full px-6 lg:px-8 flex flex-col-reverse">
+        {currentPrivateMessages.length ? (
+          currentPrivateMessages.map((message) => (
+            <ChatMessage
+              key={message.id}
+              user={getUserById(message.from)}
+              timestamp={message.timestamp}
+              text={message.text}
+              removable={message.from == currentUser.id}
+              editable={message.from == currentUser.id}
+              onRemove={() =>
+                privateMessageStore.removePrivateMessage(message.id)
+              }
+              onEdit={(newMessage) =>
+                privateMessageStore.editPrivateMessage(message.id, newMessage)
+              }
+            />
+          ))
+        ) : (
+          <div className="h-full w-full justify-center flex flex-col items-center">
+            <div className="font-semibold text-3xl flex flex-col text-black items-center whitespace-normal">
+              <span className="mb-4">Say Hi to </span>
+              <div className="flex flex-col items-center mb-4">
+                <Avatar
+                  variant="rounded"
+                  src={targetUser.avatar}
+                  sx={{ width: "64px", height: "64px" }}
+                />
+                <span className="text-fuchsia-800 text-2xl">
+                  {targetUser.username}
+                </span>
+              </div>
+              <span>
+                <IconButton
+                  color="success"
+                  sx={{ width: "46px", height: "46px" }}
+                  onClick={sendHiMessage}
+                >
+                  <WavingHand sx={{ width: "36px", height: "36px" }} />
+                </IconButton>
               </span>
             </div>
-            <span>
-              <IconButton
-                color="success"
-                sx={{ width: "46px", height: "46px" }}
-                onClick={sendHiMessage}
-              >
-                <WavingHand sx={{ width: "36px", height: "36px" }} />
-              </IconButton>
-            </span>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </main>
   );
 };
