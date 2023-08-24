bb3ed24bebd220a40611612fd4be594dd414e622
Edenys Deniz González <holofist1@gmail.com>
2023-08-23 20:07:11 -0400

    Show new user data in hello section

--- src/sections/HelloSection/HelloSection.jsx
+++ src/sections/HelloSection/HelloSection.jsx
@@ -1,5 +1,4 @@
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Avatar } from "@mui/material";
 import useUserStore from "../../store/userStore";
 
 const HelloSection = () => {
@@ -7,22 +6,29 @@ const HelloSection = () => {
 
   return (
     <main className="grid w-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-xl font-semibold text-indigo-600 sm:text-3xl">
+      <div className="text-center flex flex-col items-center">
+        <p className="text-xl font-semibold text-fuchsia-700 sm:text-3xl">
           Hi 👋
         </p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        <h1 className="mt-1 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
           <span>{currentUser.firstName}</span>{" "}
           <span className="text-gray-600">{currentUser.lastName}</span>
         </h1>
-        <p className="mt-6 text-base leading-7 text-gray-500">
+        <Avatar
+          className="mt-10"
+          sx={{ width: 100, height: 100 }}
+          alt={currentUser.username}
+          src={currentUser.avatar}
+        />
+        <p className="mt-1 text-base leading-7 text-gray-400">
+          username:{" "}
+          <span className="text-gray-600 font-bold">
+            {currentUser.username}
+          </span>
+        </p>
+        <p className="mt-10 text-xl font-skin text-fuchsia-600 ">
           Your friends are waiting for you.
         </p>
-        <div className="mt-5 flex items-center justify-center gap-x-6">
-          <Link to="/">
-            <Button variant="contained">Go back home</Button>
-          </Link>
-        </div>
       </div>
     </main>
   );
