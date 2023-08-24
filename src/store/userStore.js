c72e4a321eea7cc2211af283ba05a6dc69e6ec71
Edenys Deniz González <holofist1@gmail.com>
2023-08-23 20:01:49 -0400

    Complete user info using data from register form

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,12 +1,33 @@
+import _ from "lodash";
 import { create } from "zustand";
+
+const createUsername = ({ firstName, lastName }) => {
+  return (
+    firstName.substr(0, 3).toLowerCase() +
+    lastName.substr(0, 3).toLowerCase() +
+    "test"
+  );
+};
 
 const useUserStore = create((set) => ({
   users: [],
-  currentUser: [],
+  currentUser: {},
   //TODO: complete fetch users method
   fetchAllUsers: () => set({ users: [{ name: "Julito" }] }),
   setCurrentUser: ({ firstName, lastName, email }) =>
-    set({ currentUser: { firstName, lastName, email } }),
+    set({
+      currentUser: {
+        id: 0,
+        firstName: _.capitalize(firstName),
+        lastName: _.capitalize(lastName),
+        email,
+        username: createUsername({ firstName, lastName }),
+        avatar: `https://i.pravatar.cc/100?u=${createUsername({
+          firstName,
+          lastName,
+        })}`,
+      },
+    }),
 }));
 
 export default useUserStore;
