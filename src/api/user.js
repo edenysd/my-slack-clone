7a16e9d4384d84e7b915a46c5c413f9b013c4c48
Edenys Deniz González <holofist1@gmail.com>
2023-08-24 01:39:40 -0400

    Mock user data

---
+++ src/api/user.js
@@ -0,0 +1,44 @@
+const fakeUsersData = [
+  {
+    id: 1,
+    username: "albert",
+    firstName: "Alberto",
+    lastName: "Mejías",
+    email: "alb@gmail.com",
+    avatar: "https://i.pravatar.cc/100?u=albert",
+  },
+  {
+    id: 2,
+    username: "rodolf",
+    firstName: "Rodolfo",
+    lastName: "Perez",
+    email: "rodperez@gmail.com",
+    avatar: "https://i.pravatar.cc/100?u=rodolf60",
+  },
+  {
+    id: 3,
+    username: "liset22",
+    firstName: "Liset",
+    lastName: "Cardenas",
+    email: "lisettt@gmail.com",
+    avatar: "https://i.pravatar.cc/100?u=liset22",
+  },
+  {
+    id: 4,
+    username: "mel99",
+    firstName: "Melissa",
+    lastName: "Gonzalez",
+    email: "mellisa@gmail.com",
+    avatar: "https://i.pravatar.cc/100?u=mel9",
+  },
+  {
+    id: 5,
+    username: "carlitos",
+    firstName: "Carlos",
+    lastName: "Rodriguez",
+    email: "alb@gmail.com",
+    avatar: "https://i.pravatar.cc/100?u=carlitos2",
+  },
+];
+
+export const mockUsersData = async () => fakeUsersData;
--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,19 +1,23 @@
 import _ from "lodash";
 import { create } from "zustand";
+import { mockUsersData } from "../api/user";
 
 const createUsername = ({ firstName, lastName }) => {
   return (
-    firstName.substr(0, 3).toLowerCase() +
-    lastName.substr(0, 3).toLowerCase() +
-    "test"
+    firstName.substr(0, 6).toLowerCase() + lastName.substr(0, 3).toLowerCase()
   );
 };
 
 const useUserStore = create((set) => ({
+  loadingUsersData: false,
   users: [],
   currentUser: {},
-  //TODO: complete fetch users method
-  fetchAllUsers: () => set({ users: [{ name: "Julito" }] }),
+  fetchAllUsers: () => {
+    set({ loadingUsersData: true });
+    mockUsersData().then((usersData) => {
+      set({ users: usersData, loadingUsersData: false });
+    });
+  },
   setCurrentUser: ({ firstName, lastName, email }) =>
     set({
       currentUser: {
