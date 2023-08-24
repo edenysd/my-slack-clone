2d9a24f1234ecde73c3a620d75af42d0e063641d
Edenys Deniz González <holofist1@gmail.com>
2023-08-24 12:14:04 -0400

    Mock messages api

---
+++ src/api/messages.js
@@ -0,0 +1,77 @@
+import moment from "moment";
+
+const fakeChannelsMessagesData = [
+  {
+    id: 0,
+    from: 1,
+    to: 2,
+    text: "Hiii!!! Good morning all!!!",
+    timestamp: moment().subtract(6, "days").calendar(),
+  },
+  {
+    id: 0,
+    from: 1,
+    to: 2,
+    text: "Hi friends!",
+    timestamp: moment().subtract(6, "days").calendar(),
+  },
+  {
+    id: 0,
+    from: 1,
+    to: 2,
+    text: "Hiii!!! Good morning all!!!",
+    timestamp: moment().subtract(6, "days").calendar(),
+  },
+  {
+    id: 0,
+    from: 1,
+    to: 2,
+    text: "Hiii!!! Good morning all!!!",
+    timestamp: moment().subtract(6, "days").calendar(),
+  },
+  {
+    id: 0,
+    from: 1,
+    to: 2,
+    text: "Hiii!!! Good morning all!!!",
+    timestamp: moment().subtract(6, "days").calendar(),
+  },
+];
+
+export const mockChannelsMessagesData = async () => fakeChannelsMessagesData;
+
+export const mockAddChannelMessage = async ({ from, to, text, timestamp }) => {
+  const maxId = fakeChannelsMessagesData.reduce(
+    (curMaxId, curChannel) => Math.max(curMaxId, curChannel.id),
+    0
+  );
+  fakeChannelsMessagesData.push({ id: maxId + 1, from, to, text, timestamp });
+  console.log(fakeChannelsMessagesData);
+};
+
+const fakePrivateMessagesData = [
+  {
+    id: 0,
+    from: 1,
+    to: 0,
+    text: "Hiii!!! Good morning my friend!!!",
+    timestamp: moment().subtract(6, "days").calendar(),
+  },
+];
+
+export const mockPrivateMessagesData = async () => fakePrivateMessagesData;
+
+export const mockAddPrivateMessage = async ({ from, to, text }) => {
+  const maxId = fakePrivateMessagesData.reduce(
+    (curMaxId, curChannel) => Math.max(curMaxId, curChannel.id),
+    0
+  );
+  fakePrivateMessagesData.push({
+    id: maxId + 1,
+    from,
+    to,
+    text,
+    timestamp: moment().calendar(),
+  });
+  console.log(fakePrivateMessagesData);
+};
